299243145fe428b6d1f7ffca3490e24d6968a7b1
Tristan Chauvin <tristanchauvin@gmail.com>
2021-08-25 22:34:17 -0400

    Added copyright

--- boot.py
+++ boot.py
@@ -336,4 +336,25 @@ timer.init(period=10000, mode=Timer.PERIODIC, callback=lambda t:trigger_logging(
 ## Webrepl password was set to 1234
 
 ## If a boot file was send via webrpl which crashes and prevents you from connecting
-## to the device, connect via serial port with 
+## to the device, connect via serial port with
+
+## Permissions
+# Copyright (c) 2021 Tristan Chauvin-Bossé
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
